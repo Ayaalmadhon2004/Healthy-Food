df308afb169736fe92491c8c49d796b69c9b2d21
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2025-11-26 14:04:48 +0200

    all recipe section with functionality , responise , design

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      crossOrigin="anonymous"
+      referrerPolicy="no-referrer"
+      />
+      </head>
       <body>
         <Navbar />
         <div className="pt-20">{children}</div>
