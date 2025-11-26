f24b65f022861a77fa06669d8cd13806429eaccc
Ayaalmadhon2004 <aya.alaasel123@gmail.com>
2025-11-26 10:59:45 +0200

    Responsive code , font awsome added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIv8mLwZf8U5PaO0bE8x4r4hPp0PpC5xR4T+9iWlA=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
 
 export default function RootLayout({
   children,
@@ -8,6 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      crossOrigin="anonymous"
+      referrerPolicy="no-referrer"
+      />
+    </head>
       <body>
         <Navbar />
         <div className="pt-20">{children}</div>
--- app/tips/page.jsx
+++ app/tips/page.jsx
@@ -12,28 +12,39 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="bg-[var(--color-primary-light)] w-full h-full px-36 py-4">
-      <h1 className="text-2xl font-bold">Health & Nutrition Tips</h1>
-      <p className="pb-3">Some helpful tips for a healthier life.</p>
+    <div className="bg-[var(--color-primary-light)] min-h-screen p-4 md:p-10">
+      <div className="container mx-auto">
 
-      {tips.map((item) => (
-        <div key={item.id}>
-          <h3 className="pb-3 pt-3">{item.header}</h3>
+        <h1 className="text-xl md:text-3xl font-bold">Health & Nutrition Tips</h1>
+        <p className="pb-3 text-gray-700">Some helpful tips for a healthier life.</p>
 
-          <div className="bg-[var(--color-white)] shadow-lg rounded-xl p-5">
-            <div className="flex items-center justify-between">
-              <p className="text-2xl">{item.icon}</p>
-              <span className="font-semibold">{item.header}</span>
+        {tips.map((item) => (
+          <div key={item.id} className="mb-6">
+
+            <h3 className="pt-4 pb-2 md:pt-6 md:pb-3 font-bold">{item.header}</h3>
+
+            <div className="bg-[var(--color-white)] shadow-lg rounded-xl p-4 md:p-6 w-full">
+              
+              <div className="flex items-center justify-between bg-[rgba(250, 243, 230, 1)] p-3 md:p-4 rounded-lg">
+                <p className="text-2xl">{item.icon}</p>
+                <span className="font-semibold">{item.header}</span>
+              </div>
+
+              <p className="font-medium mt-2">{item.advice}</p>
+              <p className="text-gray-500 mb-3">{item.details}</p>
+
+              <Link 
+                className="text-[var(--color-black)] no-underline bg-[var(--color-primary-light)] rounded-xl p-2 inline-block"
+                href={`/tips/${item.id}`}
+              >
+                Read more !
+              </Link>
+
             </div>
-
-            <p className="font-medium mt-2">{item.advice}</p>
-            <p className="text-gray-500">{item.details}</p>
-            <Link href={`/tips/${item.id}`} className="text-blue-600 underline mt-3 inline-block">
-              Read more !
-            </Link>
           </div>
-        </div>
-      ))}
+        ))}
+
+      </div>
     </div>
   );
 }
